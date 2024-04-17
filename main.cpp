0aa44031cc4164c1e3906e701ea217c35240ba22
María F. Martínez <16090@portalcedi.edu.mx>
2024-04-17 14:52:25 -0600

    cambiao

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
+
+
+float speedX=1.f;
+float speedY=1.f;
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
-
+    RenderWindow window(VideoMode(800, 600), "SFML works!");
+    RectangleShape shape({20.f,10.f});
+    
+    window.setFramerateLimit(5000);
     while (window.isOpen())
     {
         Event event;
@@ -16,6 +20,20 @@ int main()
         }
 
         window.clear();
+        //if(Mouse:)
+
+        if(shape.getPosition().x+shape.getSize().x>=800|| shape.getPosition().x<0){
+            
+            speedX*= -1;
+          
+        }
+        if(shape.getPosition().y+shape.getSize().y>=600|| shape.getPosition().y<0){
+            
+            speedY*= -1;
+          
+        }
+        shape.setFillColor({shape.getPosition().x,shape.getPosition().y,shape.getPosition().x});
+        shape.move({speedX,speedY});
         window.draw(shape);
         window.display();
     }
