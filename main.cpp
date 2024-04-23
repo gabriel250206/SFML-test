2ec445bd94c69e51b22ae75a9211654f4dfd03a5
María F. Martínez <16090@portalcedi.edu.mx>
2024-04-23 12:29:38 -0600

    ola

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main()
     RenderWindow window(VideoMode(800, 600), "SFML works!");
     
     
-    window.setFramerateLimit(5000);
+    window.setFramerateLimit(200);
 
 Rectangle rect(Vector2f(60.f,30.f));
 
@@ -22,23 +22,31 @@ Rectangle rect(Vector2f(60.f,30.f));
         Event event;
         while (window.pollEvent(event))
         {
+            // escucha una vez cuando se presiona el click izquierdo del mouse
             if (event.type == Event::Closed)
                 window.close();
-            if (event.type==Event::MouseButtonPresse){
-                if(event.mouseButton.button== Mouse::left){
+            if (event.type==Event::MouseButtonPressed){
+                if(event.mouseButton.button== Mouse::Left){
                     float x=float(rand()%100);
                     float y=float(rand()%100);
-                    Rectangle r= Rectangle (Vector2f(x,y))
+                    Rectangle r= Rectangle (Vector2f(x,y));
                     rectangulos.push_back(r);
                     r.drawTo(window);
                 }
-|
+
             }
         }
 
+//entra al if si el click iquerido esta presionado
+        if(Mouse::isButtonPressed(Mouse::Left));
+
         window.clear();
-        rect.update();
-        rect.drawTo(window);
+        for(auto &r: rectangulos){
+            r.update();
+            r.drawTo(window);
+        }
+        // rect.update();
+        // rect.drawTo(window);
         window.display();
     }
 
