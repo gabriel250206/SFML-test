88981c7b0bd6e6ab09a792e7d338eb6d9489d17d
María F. Martínez <16090@portalcedi.edu.mx>
2024-05-07 12:45:02 -0600

    a

--- Grid.cpp
+++ Grid.cpp
@@ -1,16 +1,72 @@
 #include "Grid.hpp"
+#include <iostream>
+using namespace std;
+Grid::Grid(int rows, int cols, int width, int height)
+{
+    this->rows = rows;
+    this->cols = cols;
+    this->sizeX=width/cols;
+    this->sizeY=height/rows;
+    for (int i = 0; i < rows; i++)
+    {
+        tablero.push_back({});
+        for (int j = 0; j < cols; j++)
+        {
+            tablero[i].push_back(0);
+        }
+    }
 
+    this->next=vector<vector<int>>(rows,vector<int>(cols,0));
 
-Grid::Grid(int rows, int cols){
-    this->rows=rows;
-    this->cols=cols;
-    for(int i=0;i<rows;i++){
-        this->grid.push_back({});
-        for(int j=0;i<cols;i++){
-            this->grid[i].push_back(0);
+}
+void Grid::drawTo(RenderWindow  &window){
+    for (int i = 0; i < this->rows; i++)
+    {
+        for (int j = 0; j < this->cols; j++)
+        {
+            RectangleShape rect(Vector2f(this->sizeX,this->sizeY));
+            rect.setPosition(Vector2f(j*this->sizeX,i*this->sizeY));
+            rect.setOutlineThickness(1);
+            rect.setOutlineColor(Color::Black);
+            if(tablero[j][i]==1){
+                rect.setFillColor(Color::Blue);
+            }else{
+                rect.setFillColor(Color::Cyan);
+            }
+            
+            window.draw(rect);
         }
     }
 }
-void Grid::drawTo(RenderWindow &window){
 
+void Grid::click(int x, int y){
+    
+    int indexX=x/this->sizeX;
+    int indexY=y/this->sizeY;
+    std::cout<<x<<" , "<<y<<endl;
+    tablero[indexX][indexY]=(tablero[indexX][indexY]+1)%2;
+}
+
+void Grid::update(){
+    for(int i=0;i<this->rows;i++){
+        for(int j=0;j<this->cols;j++){
+            
+            if (tablero[i][j]==1){
+                if(j==this->cols-1){
+                
+            }else{
+                if(tablero[i][j+1]==0){
+                    next[i][j]=0;
+                    next[i][j+1]=1;
+                }else{
+                    this->next[i][j]=1;
+                }
+                
+            }
+                
+            }
+            
+        }
+    }
+    this->tablero=this->next;
 }
--- Grid.hpp
+++ Grid.hpp
@@ -1,15 +1,24 @@
+#ifndef Grid_h
+#define Grid_h
 #include <SFML/Graphics.hpp>
 #include <vector>
-using namespace std;
 using namespace sf;
-class Grid{
-    private:
-        vector<vector<int>>grid;
-        int rows;
-        int cols;
-    public:
-        Grid(int rows, int cols);
-        void drawTo(RenderWindow &window);
+using namespace std;
 
+class Grid
+{
+private:
+    vector<vector<int>> tablero;
+    vector<vector<int>>next;
+    int rows, cols;
+    int sizeX,sizeY;
+    Vector2i size;
 
+public:
+    Grid(int rows, int cols,int width, int height);
+    void click(int x, int y);
+    void drawTo(RenderWindow  &window);
+    void update();
 };
+
+#endif
